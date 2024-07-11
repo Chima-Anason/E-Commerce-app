71267798f3a7751ea8a281ca1727b19703303c78
Chima-Anason <tblynxx@gmail.com>
2024-07-12 00:32:55 +0800

    Setup Order service

---
+++ services/order/src/main/java/com/anagraceTech/ecommerce/orderLine/OrderLine.java
@@ -0,0 +1,22 @@
+package com.anagraceTech.ecommerce.orderLine;
+
+import com.anagraceTech.ecommerce.order.Order;
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class OrderLine {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    private Integer productId;
+    private double quantity;
+}
---
+++ services/order/src/main/java/com/anagraceTech/ecommerce/orderLine/OrderLineService.java
@@ -0,0 +1,16 @@
+package com.anagraceTech.ecommerce.orderLine;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderLineService {
+
+    private final OrderLineRepository orderLineRepository;
+    private final OrderLineMapper mapper;
+    public Integer saveOrderLine(OrderLineRequest request) {
+        var order = mapper.toOrderLine(request);
+        return orderLineRepository.save(order).getId();
+    }
+}
